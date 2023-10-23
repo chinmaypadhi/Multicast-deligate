74595209cb377125a9cac7ee91f3c93ee3d5efbe
chinmay padhi <padhichinmay108@gmail.com>
2023-10-23 19:55:06 +0530

    'commit'

---
+++ Multi cast deligate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_cast_deligate
+{
+    public delegate void addDel(int x, int y);
+    //Fi a  deligate is going to hold the adress of more than one method ,is known as multiclass deligate.
+    class Program
+    {
+        public  void add(int x, int y)
+        {
+            Console.WriteLine(x+y);
+        }
+        public  void sub(int x, int y)
+        {
+            Console.WriteLine(x - y);
+        }
+        static void Main(string[] args)
+        {
+            Program objPro = new Program();
+            addDel objDel = objPro.add;
+            //Hear we have used the same Deligate object/referance To call both the method.
+             objDel += objPro.sub;
+            objDel.Invoke(12, 55);
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ Multi cast deligate/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Multi cast deligate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Multi cast deligate")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("511be1ee-ec96-4d86-97e4-5cbf6c51f472")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
